cda16fb8a87e18a1dd3da34d35cc01068f3168b0
Jacob Duba <jacob@mooshoe.me>
2023-10-03 20:13:10 -0500

    commit

--- Midterm1/tailwind.config.js
+++ Midterm1/tailwind.config.js
@@ -2,13 +2,11 @@
 module.exports = {
   content: [
     "./**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
   ],
 }
 
