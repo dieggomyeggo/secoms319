f4acb34d92e1c08e21b91ed6e7a35bf18745b2bf
Jacob Duba <jacob@mooshoe.me>
2023-10-10 19:43:37 -0500

    com

--- Midterm1/index.js
+++ Midterm1/index.js
@@ -1,14 +1,17 @@
 const navbarToggleButton = document.getElementById("navbar-toggle-button")
 const navbarMenuItems = document.getElementById("navbar-menu-items")
 
-
-// menuButton.addEventListener('click', function () {
-//     let classList = document.getElementById('nav').classList
-//     classList.toggle('hidden')
-//     classList.toggle('block')
-//   })
 navbarToggleButton.addEventListener("click", () => {
     console.log("TEST")
     navbarMenuItems.classList.toggle("hidden")
 
 })
+
+const fetchWorkouts = async () => {
+    return await fetch("./workouts.json")
+        .then(res => res.json())
+}
+
+const [push, pull, legs] = await fetchWorkouts()
+
+
