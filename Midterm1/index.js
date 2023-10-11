ff2ec95a53a3d00281cefd1841d75e986ee74576
Jacob Duba <jacob@mooshoe.me>
2023-10-10 19:47:41 -0500

    Awesome sauce

--- Midterm1/index.js
+++ Midterm1/index.js
@@ -1,10 +1,9 @@
 const navbarToggleButton = document.getElementById("navbar-toggle-button")
-const navbarMenuItems = document.getElementById("navbar-menu-items")
 
 const workouts = document.getElementById("workouts");
 
-menuButton.addEventListener('click', function () {
-    let classList = document.getElementById('nav').classList
+navbarToggleButton.addEventListener('click', function () {
+    let classList = document.getElementById('navbar-menu-items').classList
     classList.toggle('hidden')
     classList.toggle('block')
 })
