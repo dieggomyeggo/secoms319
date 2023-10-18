b9fc16d36d5ea63168f0eae0073cbbfa62bdd5a7
Jacob Duba <jacob@mooshoe.me>
2023-10-18 09:39:25 -0500

    IMprov

---
+++ Midterm1/inspect.js
@@ -0,0 +1,7 @@
+const navbarToggleButton = document.getElementById("navbar-toggle-button");
+
+navbarToggleButton.addEventListener("click", function () {
+    let classList = document.getElementById("navbar-menu-items").classList;
+    classList.toggle("hidden");
+    classList.toggle("block");
+});
