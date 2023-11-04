e01e84e35d9b8653a15d7caad733b4393cc478d7
Diego Perez <joce@iastate.edu>
2023-11-04 16:49:59 -0500

    added modal

--- Assignment02/src/App.js
+++ Assignment02/src/App.js
@@ -1,8 +1,22 @@
 import { Products } from './Products';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 import { useState } from 'react';
-
 function App() {
   const [products, setProducts] = useState(Products);
+  const [open, setOpen] = useState(false);
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
   const setCartAmount = (index, amount) => {
     // JSON.parase(JSON.stringify()) is a trick to deep copy an object
@@ -17,10 +31,25 @@ function App() {
 
   return (
     <div>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
       <div className="h-full w-96 fixed left-0 top-0 bottom-0 bg-slate-200 p-4">
         <h1 class="text-2xl text-slate-800 font-bold text-center">Assignment 2: Store</h1>
         <input class="bg-slate-300 w-full p-3 text-slate-800 rounded-lg mt-2 mb-2" placeholder='Search this store...' onChange={(input) => { setSearch(input.target.value) }}></input>
-        <button onClick={() => console.log("Do something")} className="bg-blue-600 p-3 text-lg text-white rounded-lg hover:bg-blue-500 w-full">Checkout</button>
+        <button onClick={() => setOpen(true)} className="bg-blue-600 p-3 text-lg text-white rounded-lg hover:bg-blue-500 w-full">Checkout</button>
       </div>
       <div className="grid grid-cols-4 gap-4 p-4 ml-96">
         {products.map((product, index) => {
