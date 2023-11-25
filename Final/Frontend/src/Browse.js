bc9f313527f129f295a5e505f3c153820b38bcf8
Jacob Duba <jacob@mooshoe.me>
2023-11-25 00:02:43 -0600

    Final

---
+++ Final/Frontend/src/Browse.js
@@ -0,0 +1,20 @@
+const Browse = () => {
+  return (
+    <div>
+      <div className="mt-4 p-8 bg-gray-800 rounded-2xl">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-100 md:text-5xl lg:text-6xl ">
+          Explore exercises and develop workout plans.
+        </h1>
+        <p className="text-lg font-normal text-gray-400 lg:text-xl ">
+          There are hundreds, if not thousands of different exercises a person
+          can do in the gym for their specific muscle groups. Couple that with
+          the fact that there are hundreds of millions of combinations of these
+          exercises, which can be overwhelming for beginners. We wish to take
+          that away, simplifying workout planning for every level of lifter.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Browse;
