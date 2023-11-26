00c297a83dcf0c23904ee37f92da138ff3b785da
Diego Perez <joce@iastate.edu>
2023-11-26 14:57:11 -0600

    post, delete, and get specific user done

--- Final/Backend/App.js
+++ Final/Backend/App.js
@@ -1,17 +1,14 @@
-// Author: Diego Perez
-//   ISU Netid : joceo@iastate.edu
-//   Date :  November 13, 2023
 const { MongoClient } = require("mongodb");
 
 // Mongo
 const url = "mongodb://127.0.0.1:27017";
-const dbName = "reactdata";
+const dbName = "Final";
 const client = new MongoClient(url);
 const db = client.db(dbName);
 
 var express = require("express");
 var cors = require("cors");
-var fs = require("fs");
+// var fs = require("fs");
 var bodyParser = require("body-parser");
 
 var app = express();
@@ -23,36 +20,41 @@ const host = "localhost";
 
 app.post("/createUser", async (req, res) => {
   let collection = await db.collection("users");
-  await client.connect9;
+  await client.connect();
   let newUser = req.body;
   let result = await collection.insertOne(newUser);
   res.send(result).status(204);
 });
 
-app.get("/getUsers", async (req, res) => {
+app.get("/getUsers", async (_, res) => {
   await client.connect();
-  console.log("node connected successfully to get mongodb");
+  console.log("Node connected successfully to GET MongoDB");
   const query = {};
   const results = await db
     .collection("users")
     .find(query)
+    .limit(100)
     .toArray();
   console.log(results);
   res.status(200);
   res.send(results);
 });
 
-app.get("/getuser", async (req, res) => {
+app.get("/getUser", async (req, res) => {
   await client.connect();
   console.log("node connected successfully to get mongodb");
-  const e = req.params.email;
-  const pass = req.params.password;
-  const query = { email: e, password: pass };
-  const results = await db.collection("robots").findone(query);
+  const query = req.body;
+  const results = await db.collection("users").findOne(query);
   console.log("results: ", results);
   if (!results) res.send("not found").status(404);
-  else res.send(results.status(200));
+  else res.send(results).status(200);
 });
+
+app.delete("/deleteUser", async (req, res) => {
+  const collection = db.collection("users")
+  let result = await collection.deleteOne(req.body)
+  res.send(result).status(200)
+})
 // app.get("/listRobots", async (req, res) => {
 //   await client.connect();
 //   console.log("node connected successfully to get mongodb");
