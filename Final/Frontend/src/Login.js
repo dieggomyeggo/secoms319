75606570af87571d11c9fef1385c5dab2d81f078
Diego Perez <joce@iastate.edu>
2023-11-25 21:34:40 -0600

    beep boop

--- Final/Frontend/src/Login.js
+++ Final/Frontend/src/Login.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { createUser } from "./apiRequests";
 
 /*
   This example requires some changes to your config:
@@ -20,7 +19,7 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const handleSubmit = (e, p) => {
-    getUser(e, p);
+    console.log(e, p);
   };
   return (
     <>
@@ -53,10 +52,7 @@ export default function Login() {
                   autoComplete="email"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={(e) => {
-                    console.log(e.target.value);
-                    setEmail(e.target.value);
-                  }}
+                onChange={(e) => { console.log(e.target.value); setEmail(e.target.value) }}
                 />
               </div>
             </div>
