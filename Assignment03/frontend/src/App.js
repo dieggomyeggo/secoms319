9cd7d3a29f8ac0616efdbb9c366a0262544206f0
Diego Perez <joce@iastate.edu>
2023-12-10 14:55:49 -0600

    tailwind set up

--- Assignment03/frontend/src/App.js
+++ Assignment03/frontend/src/App.js
@@ -3,21 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="mt-4 p-8 bg-gray-800 rounded-2xl">
+      <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-100 md:text-5xl lg:text-6xl ">
+        Explore exercises and develop workout plans.
+      </h1>
+      <p className="text-lg font-normal text-gray-400 lg:text-xl ">
+        There are hundreds, if not thousands of different exercises a person
+        can do in the gym for their specific muscle groups. Couple that with
+        the fact that there are hundreds of millions of combinations of these
+        exercises, which can be overwhelming for beginners. We wish to take
+        that away, simplifying workout planning for every level of lifter.
+      </p>
+    </div>
     </div>
   );
 }
